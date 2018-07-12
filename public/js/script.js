055eb8eb33ef01781f38c5a775c317aa55a85087
Alexandr Askov <aleksandr.askov@yandex.ru>
2018-07-12 23:29:13 +0700

    form validator

--- public/js/script.js
+++ public/js/script.js
@@ -1,37 +1,93 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var input = document.querySelector('form input[type="text"]');
-  input.oninput = function() {
-    validateUrl(input.value);
-  }
+'use strict';
+
+var newUserForm;
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Form #0
+  newUserForm = (function () {
+    var err = document.querySelector('#js-new-user-form .error-container');
+    var fields = [
+      {
+        input: document.forms[0].name,
+        validator: validateName,
+      }
+    ];
+    return new Form(err, fields);
+  })();
+  // Form #1
+  // TODO
 });
 
-function validateForm() {
-  var url = document.forms[0].url.value;
-  if (validateUrl(url)) {
-    return true;
+var Form = function (errorContainer, fields) {
+  this.errors = [];
+  this.errorContainer = errorContainer;
+  this.fields = this.initFields(fields);
+};
+Form.prototype.initFields = function (fields) {
+  var tmp = [];
+  for (var i = 0; i < fields.length; i++) {
+    tmp.push(new FormInput(fields[i]));
+    this.initInputEvents(fields[i].input);
   }
-  setErrors('URL is not correct')
-  return false;
-}
-
-function validateUrl(url) {
-  if (/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.test(url)) {
-    clearErrors()
-    return true;
+  return tmp;
+};
+Form.prototype.initInputEvents = function (input) {
+  var form = this;
+  input.oninput = function () {
+    form.validate();
   }
-  return false;
-}
+};
+Form.prototype.updateErrorDisplay = function () {
+  this.errorContainer.innerHTML = this.errors[0] || '';
+};
+Form.prototype.clearErrors = function () {
+  this.errors = [];
+  this.updateErrorDisplay();
+};
+Form.prototype.addError = function (err) {
+  this.errors.push(err);
+};
+Form.prototype.validate = function () {
+  this.clearErrors();
+  for (var i = 0; i < this.fields.length; i++) {
+    const err = this.fields[i].validator(this.fields[i].el.value);
+    if (err) {
+      this.addError(err);
+    }
+  }
+  this.updateErrorDisplay();
+  return !this.errors.length;
+};
+Form.prototype.submit = function () {
+  this.error.innerHTML = '';
+};
 
-function setErrors(err) {
-  var container = document.querySelector('.error-container');
-  var input = document.querySelector('form input[type="text"]');
-  container.innerHTML = err;
-  input.className = 'input-error';
-}
 
-function clearErrors() {
-  var container = document.querySelector('.error-container');
-  var input = document.querySelector('form input[type="text"]');
-  container.innerHTML = '';
-  input.className = '';
+var FormInput = function (field) {
+  this.el = field.input;
+  this.validator = field.validator;
+  this.errClass = 'input-error';
+};
+FormInput.prototype.setErrorClass = function (err) {
+  const tmp = this.el.className.split(' ');
+  if (tmp.indexOf(this.errClass) === -1) {
+    tmp.push(this.errClass);
+  }
+  this.el.className = tmp.join(' ');
+};
+FormInput.prototype.removeErrorClass = function () {
+  const tmp = this.el.className.split(' ');
+  var index = tmp.indexOf(this.errClass);
+  if (index !== -1) {
+    tmp.splice(index, 1);
+  }
+  this.el.className = tmp.join(' ');
+};
+
+// Validators
+function validateName(name) {
+  if (!/^[a-zA-Z]\w+$/.test(name)) {
+    return '* User name must contain only alphanumeric characters, underscore and start with letter';
+  }
+  return null;
 }
